638aa83cd5d9ff2d52883411d2dd1f08e0d9a989
PabloMartins031 <184886680+PabloMartins031@users.noreply.github.com>
2026-08-11 14:36:06 -0300

    fix: configura conexão mysql no railway

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-
 import os
 
 load_dotenv()
@@ -10,14 +9,15 @@ HOST = os.getenv("MYSQL_HOST")
 USER = os.getenv("MYSQL_USER")
 PASSWORD = os.getenv("MYSQL_PASSWORD")
 DATABASE = os.getenv("MYSQL_DATABASE")
+PORT = os.getenv("MYSQL_PORT", "3306")
 
 print(f"HOST = {HOST}")
 print(f"USER = {USER}")
-print(f"PASSWORD = {PASSWORD}")
 print(f"DATABASE = {DATABASE}")
+print(f"PORT = {PORT}")
 
 DATABASE_URL = (
-    f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
+    f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 )
 
 engine = create_engine(DATABASE_URL)
